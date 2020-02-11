0a3e0eb5c2d0f4365fd248323fa59a2d4abfaaf2
Alek Tobias <alektobias@gmail.com>
2020-02-11 18:38:17 -0300

    feat: base done

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+		'airbnb-base',
+		'prettier'
+	],
+	plugins: ['prettier', 'import-helpers'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+		"prettier/prettier": "error",
+		"class-methods-use-this": "off",
+		"no-param-reassign": "off",
+		"camelcase": "off",
+		"no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+		"import-helpers/order-imports": [
+			"warn",
+			{
+					"newlinesBetween": "always", // new line between groups
+					"groups": [
+							"module",
+							"/^~/",
+							["parent", "sibling", "index"],
+					],
+					"alphabetize": { order: "asc", ignoreCase: true }
+			},
+  	],
+  },
+};
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes
+	.route('/')
+	.get((req, res) => res.json({ msg: 'Everything is running fine' }));
+
+export default routes;
