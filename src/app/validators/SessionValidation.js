abc0a1e7ab911f13d761ce50b6e602c67a0fffed
Alek Tobias <alektobias@gmail.com>
2020-02-24 11:26:49 -0300

    feat: session route added

---
+++ src/app/validators/SessionValidation.js
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+
+class SessionValidation {
+	async store(req, res, next) {
+		try {
+			const schema = Yup.object().shape({
+				email: Yup.string()
+					.email()
+					.required(),
+				password: Yup.string().required(),
+			});
+
+			await schema.validate(req.body, { abortEarly: false });
+			return next();
+		} catch (error) {
+			return res
+				.status(400)
+				.json({ error: 'Validation fails', messages: error.inner });
+		}
+	}
+}
+
+export default new SessionValidation();
