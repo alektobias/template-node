095b75b357a6db71e81c149990c9101930323215
Alek Tobias <alektobias@gmail.com>
2020-02-24 11:26:14 -0300

    fix: user store fixed

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,40 +1,49 @@
 import User from '../models/User';
 
 class UserController {
-	async store(req, res) {
-		const { email } = req.body;
-		const userExists = await User.findOne({ where: { email } });
-
-		if (userExists)
-			return res
-				.status(400)
-				.json({ error: 'This email is already registered!' });
-
-		const user = await User.create(req.body);
-
-		return res.json(user);
-	}
-
-	async update(req, res) {
-		const { email, oldPassword } = req.body;
-
-		const user = await User.findByPk(req.userId);
-
-		if (email && email !== user.email) {
+	async store(req, res, next) {
+		try {
+			const { email } = req.body;
 			const userExists = await User.findOne({ where: { email } });
 
 			if (userExists)
 				return res
 					.status(400)
-					.json({ error: 'This email is already registred.' });
+					.json({ error: 'This email is already registered!' });
+
+			const user = await User.create(req.body);
+
+			return res.json(user);
+		} catch (error) {
+			console.log(error);
+			return next(new Error(error));
 		}
+	}
 
-		if (oldPassword && !(await user.checkPassword(oldPassword)))
-			return res.status(401).json({ error: 'Password incorrect.' });
+	async update(req, res, next) {
+		try {
+			const { email, oldPassword } = req.body;
 
-		const { id, name, email: userEmail } = await user.update(req.body);
+			const user = await User.findByPk(req.userId);
 
-		return res.json({ id, name, email: userEmail });
+			if (email && email !== user.email) {
+				const userExists = await User.findOne({ where: { email } });
+
+				if (userExists)
+					return res
+						.status(400)
+						.json({ error: 'This email is already registred.' });
+			}
+
+			if (oldPassword && !(await user.checkPassword(oldPassword)))
+				return res.status(401).json({ error: 'Password incorrect.' });
+
+			const { id, name, email: userEmail } = await user.update(req.body);
+
+			return res.json({ id, name, email: userEmail });
+		} catch (error) {
+			return next(new Error(error));
+		}
 	}
 }
 
